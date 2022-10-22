740c91be30466b806a03d61024cb234a609d551c
donovan6000 <nicolasflamel@mwcwallet.com>
2022-10-21 19:04:20 -0700

    Allows accepting a fractional result size to match JavaScript behavior

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 // Header files
-#include <cmath>
 #include <cstring>
+#include <limits>
 #include <new>
 #include <node_api.h>
 #include <tuple>
@@ -83,7 +83,7 @@ napi_value compute(napi_env environment, napi_callback_info arguments) {
 	
 	// Check if getting result size from arguments failed
 	double resultSize;
-	if(napi_get_value_double(environment, argv[0], &resultSize) != napi_ok || fmod(resultSize, 1) || resultSize > SIZE_MAX) {
+	if(napi_get_value_double(environment, argv[0], &resultSize) != napi_ok || resultSize > numeric_limits<vector<uint8_t>::size_type>::max()) {
 	
 		// Return operation failed
 		return OPERATION_FAILED;
